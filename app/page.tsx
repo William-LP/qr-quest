2d85396a4f47b7b5fae0cce63b633ba3de4a8377
William Le Pommelet <william.lepommelet@powerup-technology.com>
2024-06-11 18:44:25 +0200

    remove download behavior

--- app/page.tsx
+++ app/page.tsx
@@ -28,31 +28,31 @@ const Map = dynamic(() => import('@/components/map'), { ssr: false });
 export default function Home() {
   const [checkpoints, setCheckpoints] = useState<CheckPoint[]>([]);
 
-  const onImageDownload = () => {
-    checkpoints.map((c) => {
-      const svg = document.getElementById(`qr-code-${c.id - 1}`);
-      if (svg) {
-        const svgData = new XMLSerializer().serializeToString(svg);
-        const canvas = document.createElement("canvas");
-        const ctx = canvas.getContext("2d");
-        const img = new Image();
-        img.onload = () => {
-          canvas.width = img.width;
-          canvas.height = img.height;
-          ctx?.drawImage(img, 0, 0);
-          const pngFile = canvas.toDataURL("image/png");
-          const downloadLink = document.createElement("a");
-          downloadLink.download = c.name;
-          downloadLink.href = `${pngFile}`;
-          downloadLink.click();
-        };
-        img.src = `data:image/svg+xml;base64,${btoa(svgData)}`;
-      }
-    })
-  };
+  // const onImageDownload = () => {
+  //   checkpoints.map((c) => {
+  //     const svg = document.getElementById(`qr-code-${c.id - 1}`);
+  //     if (svg) {
+  //       const svgData = new XMLSerializer().serializeToString(svg);
+  //       const canvas = document.createElement("canvas");
+  //       const ctx = canvas.getContext("2d");
+  //       const img = new Image();
+  //       img.onload = () => {
+  //         canvas.width = img.width;
+  //         canvas.height = img.height;
+  //         ctx?.drawImage(img, 0, 0);
+  //         const pngFile = canvas.toDataURL("image/png");
+  //         const downloadLink = document.createElement("a");
+  //         downloadLink.download = c.name;
+  //         downloadLink.href = `${pngFile}`;
+  //         downloadLink.click();
+  //       };
+  //       img.src = `data:image/svg+xml;base64,${btoa(svgData)}`;
+  //     }
+  //   })
+  // };
 
   const handleLaunchYourAdventure = () => {
-    onImageDownload()
+    // onImageDownload()
   }
 
 
